b9313e7fc5954319a9a3f2af2474978472d2b84d
Michael Donovan <mike@eggborne.com>
2018-08-31 20:51:21 -0700

    Create end-of-game sequence, fix refresh function, refine animations

--- js/scripts.js
+++ js/scripts.js
@@ -5,111 +5,104 @@ function Card(suit, rank) {
   this.dimensions = {
     width: 225,
     height: 315
+  };
+  this.value = this.getValue();
+}
+Card.prototype.getValue = function(){
+  return table.ranks.slice().reverse().indexOf(this.rank);
+}
+Card.prototype.showFront = function() {
+  var pos = {};
+  pos.left = table.ranks.indexOf(this.rank) * this.dimensions.width;
+  pos.top = table.suits.indexOf(this.suit) * this.dimensions.height;
+  this.div.css({
+    'background-image': 'url(img/cardsheet.png)',
+    'background-size': (this.dimensions.width*13)+'px '+(this.dimensions.height*4)+'px',
+    'background-position': '-' + pos.left + 'px -' + pos.top + 'px'
+  });
+}
+Card.prototype.showBack = function() {
+  this.div.css({
+    'background-image': 'url(img/cardback.png)',
+    'background-size': (this.dimensions.width)+'px '+(this.dimensions.height)+'px',
+    'background-position': '0',
+  });
+}
+Card.prototype.place = function (targetElement,resize,faceDown,stayFlipped) {
+  var self = this;
+  targetElement.html(this.cardHTML);
+  this.div = $('#'+this.rank+`-of-`+this.suit);
+  if (resize) {
+    this.dimensions.width = targetElement.width();
+    this.dimensions.height = targetElement.height();
   }
-  var card = this;
-  this.getValue = function(){
-    return table.ranks.slice().reverse().indexOf(this.rank);
-  }
-  this.animateFlipUp = function() {
-    var self = this;
-    var pos = {};
-    pos.left = table.ranks.indexOf(self.rank) * self.dimensions.width;
-    pos.top = table.suits.indexOf(self.suit) * self.dimensions.height;
-    setTimeout(function(){
-      self.div.css({
-        'transform':'scaleX(0)'
-      });
-    },300);
-    setTimeout(function(){
-      self.div.css({
-        'background-image': 'url(img/cardsheet.png)',
-        'background-size': (self.dimensions.width*13)+'px '+(self.dimensions.height*4)+'px',
-        'background-position': '-' + pos.left + 'px -' + pos.top + 'px'
-      });
-      self.div.css({
-        'transform':'scaleX(1)'
-      });
-    },480);
-  }
-  this.place = function (targetElement,resize,faceDown,stayFlipped) {
-    targetElement.html(this.cardHTML);
-    this.div = $('#'+this.rank+`-of-`+this.suit);
-    if (resize) {
-      this.dimensions.width = targetElement.width();
-      this.dimensions.height = targetElement.height();
+  var pos = {};
+  pos.left = table.ranks.indexOf(this.rank) * this.dimensions.width;
+  pos.top = table.suits.indexOf(this.suit) * this.dimensions.height;
+  this.div.css({
+    'background-repeat': 'no-repeat',
+    'width': this.dimensions.width + 'px',
+    'height': this.dimensions.height + 'px'
+  });
+  if (faceDown) {
+    this.showBack();
+    if (!stayFlipped) {
+      setTimeout(function(){
+        self.animateFlip();
+      },200)
+    } else {
+      this.showFront()
     }
-    var pos = {};
-    pos.left = table.ranks.indexOf(this.rank) * this.dimensions.width;
-    pos.top = table.suits.indexOf(this.suit) * this.dimensions.height;
-    this.div.css({
-      'background-image': 'url(img/cardsheet.png)',
-      'background-size': (this.dimensions.width*13)+'px '+(this.dimensions.height*4)+'px',
-      'background-position': '-' + pos.left + 'px -' + pos.top + 'px',
-      'background-repeat': 'no-repeat',
-      'width': this.dimensions.width + 'px',
-      'height': this.dimensions.height + 'px'
-    });
-    this.div.animate({
-      'opacity': '1'
-    },200);
-    this.div.delay(500).removeClass('protruding')
+  }
+  this.div.animate({
+    'opacity': '1'
+  },180);
+  this.div.delay(500).removeClass('protruding')
+  if (Array.from($(targetElement)[0].classList).includes("holeCard")) {
     this.div.click(function(){
-      $(this).css({
+      self.animateFlip();
+    });
+  }
+  table.dealtCards.push(this);
+}
+Card.prototype.animateFlip = function(side="auto") {
+  var self = this;
+  if (this.div.css("background-image").includes("cardsheet")) {
+    if (side !== "front") {
+      this.div.css({
         'transform':'scaleX(0)'
       });
-      var self = this
       setTimeout(function(){
-        if ($(self).css("background-image").includes("cardsheet")) {
-          $(self).css({
-            'background-image': 'url(img/cardback.png)',
-            'background-size': (card.dimensions.width)+'px '+(card.dimensions.height)+'px',
-            'background-position': '0 0'
-          });
-        } else {
-          var pos = {};
-          pos.left = table.ranks.indexOf(card.rank) * targetElement.width();
-          pos.top = table.suits.indexOf(card.suit) * targetElement.height();
-          $(self).css({
-            'background-image': 'url(img/cardsheet.png)',
-            'background-size': (card.dimensions.width*13)+'px '+(card.dimensions.height*4)+'px',
-            'background-position': '-' + pos.left + 'px -' + pos.top + 'px'
-          });
-        }
-      },90);
-      setTimeout(function(){
-        $(self).css({
+        self.div.css({
+          'background-image': 'url(img/cardback.png)',
+          'background-size': (self.dimensions.width)+'px '+(self.dimensions.height)+'px',
+          'background-position': '0 0'
+        });
+        self.div.css({
           'transform':'scaleX(1)'
         });
       },180);
-    });
-    if (faceDown) {
-      this.toggleFlip();
-      var self = this;
-      if (!stayFlipped) {
-        self.animateFlipUp()
-      }
     }
-    table.dealtCards.push(this);
-  }
-  this.toggleFlip = function() {
-    if (this.div.css("background-image").includes("cardsheet")) {
+  } else if (this.div.css("background-image").includes("cardback")) {
+    if (side !== "back") {
       this.div.css({
-        'background-image': 'url(img/cardback.png)',
-        'background-size': (this.dimensions.width)+'px '+(this.dimensions.height)+'px',
-        'background-position': '0',
+        'transform':'scaleX(0)'
       });
-    } else {
       var pos = {};
-      pos.left = table.ranks.indexOf(this.rank) * this.dimensions.width;
-      pos.top = table.suits.indexOf(this.suit) * this.dimensions.height;
-      this.div.css({
-        'background-image': 'url(img/cardsheet.png)',
-        'background-size': (this.dimensions.width*13)+'px '+(this.dimensions.height*4)+'px',
-        'background-position': '-' + pos.left + 'px -' + pos.top + 'px'
-      });
+      pos.left = table.ranks.indexOf(self.rank) * self.dimensions.width;
+      pos.top = table.suits.indexOf(self.suit) * self.dimensions.height;
+      setTimeout(function(){
+        self.div.css({
+          'background-image': 'url(img/cardsheet.png)',
+          'background-size': (self.dimensions.width*13)+'px '+(self.dimensions.height*4)+'px',
+          'background-position': '-' + pos.left + 'px -' + pos.top + 'px'
+        });
+        self.div.css({
+          'transform':'scaleX(1)'
+        });
+      },180);
     }
   }
-  this.value = this.getValue();
 }
 function Hand(arr) {
   this.cards = arr;
@@ -197,13 +190,12 @@ Player.prototype.changeBankAmountBy = function (amount) {
   console.log("adding " + amount)
   this.bank += amount;
 }
-
 $(document).ready(function() {
   $("#enterName").submit(function(event) {
     event.preventDefault();
     $(".sign-in").hide();
     $("#table").show();
-    var names = [($("#name1").val() || "Player 1" ), ( $("#name2").val() || "Player 2" )];
+    var names = [ name1.value || "Player 1" , name2.value || "Player 2" ];
     table.initiateGame(names);
   });
   $('#call-check').click(function(){
@@ -290,12 +282,11 @@ window.addEventListener("resize", function() {
   });
 });
 document.onkeydown = function(event) {
-  event.preventDefault();
   if (event.keyCode == 32 && $('#space-for-next').css('display') !== 'none') {
+    event.preventDefault();
     table.startNewHand()
     console.log($('#space-for-next').css('opacity'))
   }
-  // arrow keys for swipe actions?
 };
 window.addEventListener("input",function(event){
   if (event.target.id === "funds") {
@@ -304,104 +295,5 @@ window.addEventListener("input",function(event){
     } else {
       $('#bet-raise').text("Raise " + $('#funds').val());
     }
-    
   }
 });
-
-// Function just for testing purposes, will shuffle the deck, create a five card hand object.
-// function fiveCardHand() {
-//   var newDeck = table.shuffle();
-//   var cards = [];
-//   for (i = 0; i <= 4; i++) {
-//     cards.push(newDeck.pop());
-//   }
-//   var hand = new Hand(cards);
-//   return hand;
-// }
-// Pre-built common hands for testing purposes.
-
-// card1 = new Card("diamonds", "ace");
-// card2 = new Card("diamonds", "king");
-// card3 = new Card("diamonds", "queen");
-// card4 = new Card("diamonds", "jack");
-// card5 = new Card("diamonds", "ten");
-// reallyGoodCards = [card1, card2, card3, card4, card5];
-// royalFlush = new Hand(reallyGoodCards);
-
-// card1 = new Card("diamonds", "two");
-// card2 = new Card("diamonds", "seven");
-// card3 = new Card("diamonds", "nine");
-// card4 = new Card("diamonds", "four");
-// card5 = new Card("diamonds", "queen");
-// reallyGoodCards = [card1, card2, card3, card4, card5];
-// flush = new Hand(reallyGoodCards);
-
-// card1 = new Card("clubs", "five");
-// card2 = new Card("diamonds", "six");
-// card3 = new Card("spades", "seven");
-// card4 = new Card("diamonds", "eight");
-// card5 = new Card("hearts", "nine");
-// reallyGoodCards = [card1, card2, card3, card4, card5];
-// straight = new Hand(reallyGoodCards);
-
-// card1 = new Card("diamonds", "three");
-// card2 = new Card("clubs", "three");
-// card3 = new Card("spades", "three");
-// card4 = new Card("diamonds", "eight");
-// card5 = new Card("spades", "eight");
-// reallyGoodCards = [card4, card3, card1, card2, card5];
-// fullHouse = new Hand(reallyGoodCards);
-
-// card1 = new Card("diamonds", "five");
-// card2 = new Card("clubs", "five");
-// card3 = new Card("hearts", "five");
-// card4 = new Card("spades", "five");
-// card5 = new Card("diamonds", "king");
-// reallyGoodCards = [card1, card2, card5, card3, card4];
-// fourOfAKind = new Hand(reallyGoodCards);
-//
-// card1 = new Card("diamonds", "six");
-// card2 = new Card("clubs", "six");
-// card3 = new Card("hearts", "six");
-// card4 = new Card("spades", "six");
-// card5 = new Card("diamonds", "king");
-// reallyGoodCards = [card1, card2, card5, card3, card4];
-// fourOfAKind2 = new Hand(reallyGoodCards);
-
-// card1 = new Card("diamonds", "five");
-// card2 = new Card("diamonds", "six");
-// card3 = new Card("diamonds", "seven");
-// card4 = new Card("diamonds", "eight");
-// card5 = new Card("diamonds", "nine");
-// reallyGoodCards = [card1, card2, card3, card4, card5];
-// straightFlush = new Hand(reallyGoodCards);
-//
-// card1 = new Card("diamonds", "five");
-// card2 = new Card("diamonds", "six");
-// card3 = new Card("diamonds", "seven");
-// card4 = new Card("diamonds", "eight");
-// card5 = new Card("diamonds", "nine");
-// reallyGoodCards = [card1, card2, card3, card4, card5];
-// straightFlush = new Hand(reallyGoodCards);
-//
-// card1 = new Card("diamonds", "five");
-// card2 = new Card("clubs", "five");
-// card3 = new Card("diamonds", "seven");
-// card4 = new Card("spades", "seven");
-// card5 = new Card("hearts", "two");
-// reallyGoodCards = [card1, card5, card2, card4, card3];
-// twoPair = new Hand(reallyGoodCards);
-//
-// card1 = new Card("diamonds", "ace");
-// card2 = new Card("clubs", "jack");
-// card3 = new Card("diamonds", "king");
-// card4 = new Card("hearts", "ace");
-// card5 = new Card("hearts", "three");
-// reallyGoodCards = [card1, card2, card3, card4, card5];
-// pair = new Hand(reallyGoodCards);
-// //
-// //
-// //
-// card6 = new Card("spades", "jack");
-// card7 = new Card("diamonds", "four");
-// sevenCardArr = [card1, card2, card3, card4, card5, card6, card7]
